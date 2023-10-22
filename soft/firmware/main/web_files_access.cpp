15a8958e8516eb34db952fbe5aa7f87618f92644
Roman Khvatov <rakhvato@gmail.com>
2023-10-22 21:18:36 +0100

    Updated script output

--- soft/firmware/main/web_files_access.cpp
+++ soft/firmware/main/web_files_access.cpp
@@ -10,6 +10,8 @@ CDNDef decode_web_files_access_function(const char* key)
     extern const unsigned char web_admin_html_end[] asm("_binary_web_admin_html_end");
     extern const unsigned char web_edit_user_html_start[] asm("_binary_web_edit_user_html_start");
     extern const unsigned char web_edit_user_html_end[] asm("_binary_web_edit_user_html_end");
+    extern const unsigned char web_fg_editor_html_start[] asm("_binary_web_fg_editor_html_start");
+    extern const unsigned char web_fg_editor_html_end[] asm("_binary_web_fg_editor_html_end");
     extern const unsigned char web_fg_viewer_html_start[] asm("_binary_web_fg_viewer_html_start");
     extern const unsigned char web_fg_viewer_html_end[] asm("_binary_web_fg_viewer_html_end");
     extern const unsigned char web_jslib_js_start[] asm("_binary_web_jslib_js_start");
@@ -46,7 +48,23 @@ CDNDef decode_web_files_access_function(const char* key)
                 default: return CDNDef{};
             }
         case 'e': return strcmp(key+1, "dit_user.html") == 0 ? CDNDef{web_edit_user_html_start, web_edit_user_html_end} : CDNDef{};
-        case 'f': return strcmp(key+1, "g_viewer.html") == 0 ? CDNDef{web_fg_viewer_html_start, web_fg_viewer_html_end} : CDNDef{};
+        case 'f':
+            switch(key[1])
+            {
+                case 'g':
+                    switch(key[2])
+                    {
+                        case '_':
+                            switch(key[3])
+                            {
+                                case 'e': return strcmp(key+4, "ditor.html") == 0 ? CDNDef{web_fg_editor_html_start, web_fg_editor_html_end} : CDNDef{};
+                                case 'v': return strcmp(key+4, "iewer.html") == 0 ? CDNDef{web_fg_viewer_html_start, web_fg_viewer_html_end} : CDNDef{};
+                                default: return CDNDef{};
+                            }
+                        default: return CDNDef{};
+                    }
+                default: return CDNDef{};
+            }
         case 'j': return strcmp(key+1, "slib.js") == 0 ? CDNDef{web_jslib_js_start, web_jslib_js_end} : CDNDef{};
         case 'l': return strcmp(key+1, "oad_gifts.html") == 0 ? CDNDef{web_load_gifts_html_start, web_load_gifts_html_end} : CDNDef{};
         case 'm': return strcmp(key+1, "essage.html") == 0 ? CDNDef{web_message_html_start, web_message_html_end} : CDNDef{};
