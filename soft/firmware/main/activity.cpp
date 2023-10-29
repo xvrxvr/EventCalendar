668195c10bd27a47dbb2f993b1a008e9d0b80dc5
Roman Khvatov <rakhvato@gmail.com>
2023-10-29 20:17:31 +0000

    Cont development of Activity manager

--- soft/firmware/main/activity.cpp
+++ soft/firmware/main/activity.cpp
@@ -1,50 +1,231 @@
+#include <string.h>
+
 #include "activity.h"
 #include "input_hw.h"
+
+static constexpr int MAX_ACTIVITIES = 4;
 
 static TouchInput touch_input("Input-Touch");
 static FGInput fg_input("Input-FG");
 
-Activity::Activity(int /* bitset of ActionType and ActionFlags*/ actions, int /* bitset of ActionType*/ can_be_borrowed_actions)
-{
+static Activity* all_activities[MAX_ACTIVITIES];
 
+static SemaphoreHandle_t activity_lock;
+
+class L {
+public:
+    L() {xSemaphoreTake(activity_lock, portMAX_DELAY);}
+    ~L() {xSemaphoreGive(activity_lock);}
+};
+
+Activity::Activity(uint32_t /* bitset of ActionType and ActionFlags*/ actions, uint32_t /* bitset of ActionType*/ can_be_borrowed_actions) : actions(actions), can_be_borrowed_actions(can_be_borrowed_actions)
+{
+    actions_queue = xQueueCreate(16, sizeof(Action));
+    L lock;
+    int empty_idx = -1;
+    for(int i=MAX_ACTIVITIES; --i;)
+    {
+        auto a = all_activities[i];
+        if (!a) {empty_idx = i; continue;}
+        if (a->actions & actions & (AT_Fingerprint|AT_TouchBit)) {status_ok = false; locked_out |= 1<< i;}
+        if (a->can_be_borrowed_actions & can_be_borrowed_actions) {status_ok = false; actions &= ~AF_Override; break;} // This is definitely error - Override will not work
+    }
+    if (!(actions & AF_Override))
+    {
+        assert(status_ok || (actions & AF_CanFail));
+    }
+    assert(empty_idx != -1);
+    all_activities[empty_idx] = this;
+    my_index = empty_idx;
+    if (!status_ok)
+    {
+        if (!(actions & AF_Override)) return; // This is error case - just return
+        // Otherwise - lock out all conflicting
+        status_ok = true;
+        for(int i=0; i<MAX_ACTIVITIES; ++i)
+        {
+            auto a = all_activities[i];
+            if ((locked_out >> i) & 1) a->lock_out(); else
+            if (a && (actions & a->can_be_borrowed_actions)) // borrow something
+            {
+                a->borrowed |= actions & a->can_be_borrowed_actions;
+                a->push_spc_code(IA_Borrow, actions & a->can_be_borrowed_actions);
+            }
+        }
+    }
 }
 
-Activity::~Activity() {}
-
-void Activity::setup_alarm_action(uint32_t time_to_hit)
+void Activity::lock_out()
 {
+    suspended = true;
+    push_spc_code(IA_Suspend);
+    if (alarm_timer) xTimerStop(alarm_timer, portMAX_DELAY);
+    if (watchdog_timer) xTimerStop(watchdog_timer, portMAX_DELAY);
 }
 
-void Activity::setup_timeout(uint32_t timeout)
+void Activity::unlock_out()
 {
+    suspended = false;
+    push_spc_code(IA_Resume);
+    if (alarm_timer) setup_alarm_action(setup_alarm_time);
+    if (watchdog_timer) setup_watchdog(setup_watchdog_time);
 }
 
-void Activity::setup_web_ping_type(const char* tag)
+Activity::~Activity() 
 {
-
+    if (alarm_timer) xTimerDelete(alarm_timer, portMAX_DELAY);
+    if (watchdog_timer) xTimerDelete(watchdog_timer, portMAX_DELAY);
+    vQueueDelete(actions_queue);
+    {
+        L lock;
+        all_activities[my_index] = NULL;
+        if (status_ok) // Unlock (if any) and returns
+        {
+            for(int i=0; i<MAX_ACTIVITIES; ++i)
+            {
+                auto a = all_activities[i];
+                if ((locked_out >> i) & 1) a->lock_out(); else
+                if (a && (actions & a->can_be_borrowed_actions)) // borrow something
+                {
+                    a->borrowed &= ~(actions & a->can_be_borrowed_actions);
+                    a->push_spc_code(IA_Return, actions & a->can_be_borrowed_actions);
+                }
+            }
+        }
+    }
 }
 
-void Activity::set_special_color_feedback_code(int)
-{
+TickType_t hit_to_ticks(uint32_t);
 
+Activity& Activity::setup_alarm_action(uint32_t time_to_hit)
+{
+    check();
+    setup_alarm_time = time_to_hit;
+    if (!(actions & AT_Alarm)) return *this;
+    if (!alarm_timer)
+    {
+        alarm_timer = xTimerCreate("Activity-Alarm", hit_to_ticks(time_to_hit), 0, this, &Activity::alarm_proxy);
+        xTimerStart(alarm_timer, portMAX_DELAY);
+    }
+    else
+    {
+        xTimerChangePeriod(alarm_timer, hit_to_ticks(time_to_hit), portMAX_DELAY);
+    }
+    return *this;
 }
 
-// You SHOULD call this method if AF_CanFail was specified in Activity::Activity(). Otherwise get_action will fail with abort()
-// Method returns true if this Activity successfully lock all required Actions.
-bool Activity::is_ok() const
+Activity& Activity::setup_watchdog(uint32_t timeout)
 {
-    return true;
+    check();
+    setup_watchdog_time = timeout;
+    auto tout = setup_watchdog_time * 1000 / portTICK_PERIOD_MS;
+    if (!(actions & AT_WatchDog)) return *this;
+    if (!watchdog_timer)
+    {
+        watchdog_timer = xTimerCreate("Activity-Watchdog", tout, 0, this, &Activity::watchdog_proxy);
+        xTimerStart(watchdog_timer, portMAX_DELAY);    
+    }
+    else
+    {
+        xTimerReset(watchdog_timer, portMAX_DELAY);   
+    }
+    return *this;
 }
 
 Action Activity::get_action() //Return input action. Blocks until action will be available.
 {
-    return {};
+    Action result;
+    check();
+    for(;;)
+    {
+        auto actions = active_actions();
+        if (actions & AT_Fingerprint)
+        {
+            uint32_t col = 0;
+            switch(actions & AT_Fingerprint2)
+            {
+
+                case AT_Fingerprint: col = FP_NORMAL_COLOR; break;
+                case AT_Fingerprint0: col = FP_NO_COLOR; break;
+                case AT_Fingerprint1: col = FP_OOB_COLOR; break;
+                case AT_Fingerprint2: col = custom_fg_color; break;
+            }
+            fg_input.cmd(col);
+        }
+        if (actions & AT_TouchBit) fg_input.cmd(actions & (AT_TouchBit|AT_TouchDown|AT_TouchUp|AT_TouchTrack));
+
+        xQueueReceive(actions_queue, &result, portMAX_DELAY);
+        if (result.type == AT_Internal)
+        {
+            switch(result.internal.code)
+            {
+                case IA_Suspend: on_suspend(); break;
+                case IA_Resume: on_resume(LCDAccess(this).access()); break;
+                case IA_Borrow: on_action_borrow(ActionType(result.internal.p1)); break;
+                case IA_Return: on_action_return(ActionType(result.internal.p1)); break;
+            }
+            continue;
+        }
+    }
+    return result;
+}
+
+void Activity::push_action(const Action& action)
+{
+    L lock;
+    for(int i=0; i<MAX_ACTIVITIES; ++i)
+    {
+        auto a = all_activities[i];
+        if (!a) continue;
+        if (a->active_actions() & action.type) a->push_action_local(action);
+    }
+}
+
+void Activity::on_web_ping_echo(const char* tag) // Global entry - dispatched to all active WEB ping sources
+{
+    L lock;
+    for(int i=0; i<MAX_ACTIVITIES; ++i)
+    {
+        auto a = all_activities[i];
+        if (!a || !a->web_ping_tag || a->locked_out || strcmp(tag, a->web_ping_tag)) continue;
+        a->web_ping_counter = 0;        
+    }
+}
+
+void send_web_ping_to_ws(const char*);
+
+void Activity::send_web_ping() // Internal function - called by WEB ping thread in this module.
+{
+    L lock;
+    for(int i=0; i<MAX_ACTIVITIES; ++i)
+    {
+        auto a = all_activities[i];
+        if (!a || !a->web_ping_tag || a->locked_out) continue;
+        send_web_ping_to_ws(a->web_ping_tag);
+        ++a->web_ping_counter;
+        if (a->web_ping_counter > MAX_WEB_PINGS)
+        {
+            a->push_action_local({.type = AT_WEBEvent, .web = {.event = WE_Logout, .logout_tag = a->web_ping_tag}});
+            a->web_ping_counter = 0;
+        }
+    }
+}
+
+static void web_ping_task(void*)
+{
+    for(;;)
+    {
+        Activity::send_web_ping();
+        vTaskDelay(WEB_PING_INTERVAL_MS / portTICK_PERIOD_MS);
+    }
 }
 
 void Activity::start()
 {
+    activity_lock = xSemaphoreCreateMutex();
     touch_input.start();
     fg_input.start();
+    xTaskCreate(web_ping_task, "WEB-Ping", WEB_PING_TASK_STACK, NULL, WEB_PING_TASK_PRIO, NULL);
 }
 
 LCDAccess::LCDAccess(Activity* a)
--- soft/firmware/main/activity.h
+++ soft/firmware/main/activity.h
@@ -1,10 +1,28 @@
 #pragma once
 
 #include <stdint.h>
+#include <assert.h>
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/timers.h>
+#include <freertos/semphr.h>
 
 #include "hadrware.h"
 
+// !!!
+enum {
+    FP_NORMAL_COLOR = 0x1234,
+    FP_NO_COLOR = 0x3456,
+    FP_OOB_COLOR = 0x3333
+};
+
+static constexpr int MAX_WEB_PINGS = 2;
+static constexpr int WEB_PING_INTERVAL_MS = 500;
+static constexpr int WEB_PING_TASK_STACK = 1024;
+static constexpr int WEB_PING_TASK_PRIO = 1;
+
 enum ActionType {
+    AT_Internal        = 0, // Internal
     AT_Fingerprint     = 0x0001,   // fingerprint with normal color feedback (and FP bit)
     AT_Fingerprint0    = 0x0003,   // fingerprint without color feedback
     AT_Fingerprint1    = 0x0005,   // fingerprint with OOB color feedback
@@ -13,7 +31,7 @@ enum ActionType {
     AT_TouchDown       = 0x0018,
     AT_TouchUp         = 0x0028,
     AT_TouchTrack      = 0x0048,
-    AT_Timeout         = 0x0080,
+    AT_WatchDog        = 0x0080,
     AT_WEBEvent        = 0x0100,
     AT_Alarm           = 0x0200,
 
@@ -53,26 +71,80 @@ struct Action {
                 const char* logout_tag;
             };
         } web;
+        struct {
+            int code;
+            int p1;
+        } internal;
     };
 };
 
 class Activity {
+    enum InternalAction {
+        IA_Suspend,
+        IA_Resume,
+        IA_Borrow,
+        IA_Return
+    };
+    bool status_ok = true; // Set to true if all Activity sources was successfully locked
+    bool status_checked = false; // Set to true if is_ok() was called
+    int my_index = -1; // Index of this Activity in array of Activities
+    uint32_t actions; // Set of Actions belong to this Activity
+    uint32_t can_be_borrowed_actions; // Set of Actions which can be borrowed
+    uint32_t borrowed = 0; // Set of Actions that was really borrowed
+    uint32_t locked_out = 0; // Set of indexes of other Activities that was locked out by this one (by AF_Override option)
+    bool suspended = false; // Set to true if this Activity was locked out by another
+    uint32_t custom_fg_color = 0; // Custom color for AT_Fingerprint2 Action
+    const char* web_ping_tag = NULL;
+    int web_ping_counter = 0; // Incremented on each 'ping' send to WEB, reset on each 'ping' echo from web. If this counter reached threshold limit - WEB timeout fired
+
+    uint32_t setup_alarm_time;
+    uint32_t setup_watchdog_time;
+
+    TimerHandle_t alarm_timer = NULL;
+    TimerHandle_t watchdog_timer = NULL;
+    QueueHandle_t actions_queue = NULL;
+
+    void reset_watchdog() {if (watchdog_timer) xTimerReset(watchdog_timer, portMAX_DELAY);}
+    void check()
+    {
+        assert(status_checked);
+        assert(status_ok);
+    }
+
+    void lock_out();
+    void unlock_out();
+
+    void on_alarm() {push_action_local({.type=AT_Alarm});}
+    void on_watchdog() {push_action_local({.type=AT_WatchDog});}
+
+    static void alarm_proxy(TimerHandle_t t) {((Activity*)pvTimerGetTimerID(t))->on_alarm();}
+    static void watchdog_proxy(TimerHandle_t t) {((Activity*)pvTimerGetTimerID(t))->on_watchdog();}
+
+    void push_action_local(const Action& a) 
+    {
+        xQueueSend(actions_queue, &a, portMAX_DELAY);
+    }
+
+    uint32_t active_actions() {return suspended ? 0 : actions | (can_be_borrowed_actions & ~borrowed);}
+
+    void push_spc_code(int code, int arg=0) { push_action_local({.type = AT_Internal, .internal = {.code = code, .p1 = arg}});}
+
 public:
-    // Create Activity anl lock all required Actions
+    // Create Activity and lock all required Actions
     // If lock was unsuccessfull and AF_CanFail not specified - abort execution
-    Activity(int /* bitset of ActionType and ActionFlags*/ actions, int /* bitset of ActionType*/ can_be_borrowed_actions=0);
+    Activity(uint32_t /* bitset of ActionType and ActionFlags*/ actions, uint32_t /* bitset of ActionType*/ can_be_borrowed_actions=0);
     ~Activity();
 
     // Required Activity setup. Appropriate members must be called BEFORE call to get_action()
     // Multiple calls to these methods possible - later call will override setup from former.
-    void setup_alarm_action(uint32_t time_to_hit);
-    void setup_timeout(uint32_t timeout);
-    void setup_web_ping_type(const char* tag);
-    void set_special_color_feedback_code(int);
+    Activity& setup_alarm_action(uint32_t time_to_hit);
+    Activity& setup_watchdog(uint32_t timeout);
+    Activity& setup_web_ping_type(const char* tag) {web_ping_tag = tag; return *this;}
+    Activity& set_special_color_feedback_code(uint32_t color) {custom_fg_color = color; return *this;}
 
     // You SHOULD call this method if AF_CanFail was specified in Activity::Activity(). Otherwise get_action will fail with abort()
     // Method returns true if this Activity successfully lock all required Actions.
-    bool is_ok() const;
+    bool is_ok() {status_checked = true; return status_ok;}
 
     Action get_action(); //Return input action. Blocks until action will be available.
 
@@ -87,6 +159,12 @@ public:
     // Initialize all Activity system, starts background tasks which handles Touch/LCD and FG
     // After this call all access to LCD/Touch/FG only through LCDAccess/FPAccess
     static void start();
+
+////////////
+// Method(s) for Activity sources
+    static void push_action(const Action&); // Global entry - dispatched for particular Activity inside
+    static void on_web_ping_echo(const char* tag); // Global entry - dispatched to all active WEB ping sources
+    static void send_web_ping(); // Internal function - called by WEB ping thread in this module.
 };
 
 
